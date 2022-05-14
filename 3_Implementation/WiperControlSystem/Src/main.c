fb0ff35b20869f970c158325200d933ac3854a54
SHIVAPRADEEP P <shivapradeep.p.2018.eee@rajalakshmi.edu.in>
2022-05-14 20:02:31 +0530

    added

--- 3_Implementation/WiperControlSystem/Src/main.c
+++ 3_Implementation/WiperControlSystem/Src/main.c
@@ -23,7 +23,7 @@ void delay(int a)
 {
 	for (uint32_t i=0;i<a;i++);
 }
-int message=1;
+int count=1,v=0;
 int main(void)
 {
 	    GPIO_Handle_t Led,INP;
@@ -63,18 +63,24 @@ int main(void)
 		INP.GPIO_PinConfig.GPIO_PinMode=GPIO_MODE_IN;
 		GPIO_Init(&INP);
 
-		while(1)
-		{
+		while(1){
+//		if(count!=1 && count<7){
+//			GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+//		}
 			if(GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0)==ENABLE){
-				if(message==1){
-					GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
-					if(GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0)==ENABLE)
-					{
-			        message++;
-					}
+				if(count==1){
+					if(GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0)==ENABLE){
+						delay(100000);
+						delay(100000);
+						delay(100000);
+						delay(100000);
+						GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+						delay(1000000);
+						//if(GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0))
+						 count++;}
 			        }
-               if(message==2){
-					while(message==2){
+				else if(count==2){
+				  while(count==2){
 			   GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
 			       delay(30000000);
 			     GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_15,0);
@@ -91,12 +97,12 @@ int main(void)
 			       delay(30000000);
 			     GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_15,0);
 			     if(GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0)==ENABLE){
-			     message++;}
+			     count++;}
 				}
 			}
-			if(message==3){
-					while(message==3){
-					GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+				else if(count==3){
+					while(count==3){
+						GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
 					     delay(20000000);
 					  GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_15,0);
 					GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
@@ -112,11 +118,11 @@ int main(void)
 			             delay(20000000);
 			          GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_15,0);
 			          if(GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0)==ENABLE){
-					     message++;}
+					     count++;}
 				}
 			}
-				if(message==4){
-					while(message==4){
+				else if(count==4){
+					while(count==4){
 					GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
 					     delay(10000000);
 					  GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_15,0);
@@ -129,17 +135,34 @@ int main(void)
 					GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
 			             delay(10000000);
 			          GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_12,0);
-			        GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
-			             delay(10000000);
-			          GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_15,0);
+
 			          if(GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0)==ENABLE){
-					message++;}
+					count++;}
 					}
 				}
-                if(message==5){
-                	delay(50000000);
-                	GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_14,0);
+				else if(count==5){
+					GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+					     delay(1000000);
+					  GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_13,0);
+            		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+						 delay(1000000);
+					GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_12,0);
+					 GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+						delay(1000000);
+				    GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_15,0);
+				    delay(1000000);
+				    if(GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0)==ENABLE){
+						count++;}
                 }
+				else if(count==6){
+					delay(50000000);
+					delay(10000000);
+					//GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0,1)
+					GPIO_WriteToOutputPin(GPIOD,GPIO_PIN_NO_14,0);
+                	count++;
+                }
+				else
+				 break;
 		}
 	}
 
